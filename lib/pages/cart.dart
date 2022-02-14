67644f685091884b22bd5791c35dee178b8fe17c
metaspook <metaspook@gmail.com>
2022-02-14 19:59:31 +0600

    update cart page

--- lib/pages/cart.dart
+++ lib/pages/cart.dart
@@ -12,12 +12,13 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-  late List _cartItemList;
+  late List<Map<String, dynamic>> _cartItemList;
+  late List<int> _cartPriceList;
 
   @override
   void initState() {
     super.initState();
-    _cartItemList = [
+    _cartItemList = <Map<String, dynamic>>[
       {
         "id": 1,
         "name": "Apple",
@@ -49,6 +50,11 @@ class _CartPageState extends State<CartPage> {
             "https://raw.githubusercontent.com/metaspook/json_api/main/images/foods/breadsticks.png"
       }
     ];
+    _cartPriceList = List<int>.generate(
+      _cartItemList.length,
+      (i) => Random().nextInt(100),
+      growable: false,
+    );
   }
 
   @override
@@ -57,38 +63,73 @@ class _CartPageState extends State<CartPage> {
       appBar: AppBar(
         centerTitle: true,
         title: Text(widget.title),
+        actions: [
+          ElevatedButton.icon(
+            onPressed: () => setState(() {
+              _cartItemList.clear();
+            }),
+            icon: Icon(
+              CupertinoIcons.delete,
+              color: Colors.orange,
+            ),
+            label: Text('Remove Cart'),
+          ),
+        ],
       ),
       body: Column(
-        children: [
+        children: <Widget>[
           ListView.builder(
               shrinkWrap: true,
               itemCount: _cartItemList.length,
               itemBuilder: (context, index) {
                 return Card(
                   child: ListTile(
-                    leading: Image.network(
-                      _cartItemList[index]['image'],
-                      fit: BoxFit.cover,
+                    leading: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          // padding: EdgeInsets.all(1),
+                          onPressed: () {
+                            setState(() {
+                              _cartItemList.removeAt(index);
+                            });
+                          },
+                          icon: Icon(
+                            CupertinoIcons.cart_badge_minus,
+                            color: Colors.orange,
+                          ),
+                        ),
+                        Image.network(
+                          _cartItemList[index]['image'],
+                          fit: BoxFit.fill,
+                        ),
+                      ],
                     ),
+                    // Image.network(
+                    //   _cartItemList[index]['image'],
+                    //   fit: BoxFit.cover,
+                    // ),
                     title: Text(
                       _cartItemList[index]['name'],
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey.shade600),
                       textAlign: TextAlign.center,
                     ),
-                    subtitle: Text(
-                      '\$${Random().nextInt(100)}',
+                    // subtitle:
+                    // Text(
+                    //   '\$${Random().nextInt(100)}',
+                    //   style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    //       fontWeight: FontWeight.bold,
+                    //       color: Theme.of(context).colorScheme.secondary),
+                    //   textAlign: TextAlign.center,
+                    // ),
+                    trailing: Text(
+                      '\$${_cartPriceList[index]}',
+                      style: Theme.of(context).textTheme.headline6!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.secondary),
                       textAlign: TextAlign.center,
-                    ),
-                    trailing: IconButton(
-                      // padding: EdgeInsets.all(1),
-                      onPressed: () {
-                        setState(() {
-                          _cartItemList.removeAt(index);
-                        });
-                      },
-                      icon: Icon(
-                        CupertinoIcons.cart_badge_minus,
-                        color: Colors.orange,
-                      ),
                     ),
                   ),
                 );
@@ -99,19 +140,26 @@ class _CartPageState extends State<CartPage> {
               ElevatedButton.icon(
                 onPressed: null,
                 icon: Icon(
-                  CupertinoIcons.delete,
-                  color: Colors.orange,
-                ),
-                label: Text('Remove Cart'),
-              ),
-              ElevatedButton.icon(
-                onPressed: null,
-                icon: Icon(
                   CupertinoIcons.text_badge_checkmark,
                   color: Colors.orange,
                 ),
                 label: Text('Checkout'),
-              )
+              ),
+              SizedBox(width: 65),
+              Text(
+                'Total:',
+                style: Theme.of(context).textTheme.headline5!.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.secondary),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                '\$${_cartPriceList.reduce((value, element) => value + element)}',
+                style: Theme.of(context).textTheme.headline4!.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.secondary),
+                textAlign: TextAlign.center,
+              ),
             ],
           ),
         ],
